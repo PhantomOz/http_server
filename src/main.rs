58ec70e98ee80fdfc91e697bfbe7eec85c268447
PhantomOz <faniogor@gmail.com>
2025-03-13 05:13:16 +0100

    This creates the httpServer struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let http_server = HTTPServer::new("0.0.0.0:8080".to_string());
+    http_server.run();
+}
+
+struct HTTPServer {
+    address: String,
+}
+
+impl HTTPServer {
+    fn new(address: String) -> Self {
+        HTTPServer { address }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.address);
+    }
 }
