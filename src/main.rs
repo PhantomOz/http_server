0083210a2d278239a9029db09c225fed60f82bc8
PhantomOz <faniogor@gmail.com>
2025-03-13 12:47:08 +0100

    This adds http_server

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
+use http_server::HTTPServer;
+
 fn main() {
     let http_server = HTTPServer::new("0.0.0.0:8080".to_string());
     http_server.run();
 }
 
-struct HTTPServer {
-    address: String,
-}
-
-impl HTTPServer {
-    fn new(address: String) -> Self {
-        HTTPServer { address }
+mod http_server {
+    pub struct HTTPServer {
+        address: String,
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.address);
+    impl HTTPServer {
+        pub fn new(address: String) -> Self {
+            HTTPServer { address }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.address);
+        }
+    }
+}
+
+mod http {
+    pub mod request {
+        use super::http_method::HTTPMethod;
+        pub struct Request {
+            method: HTTPMethod,
+            path: String,
+            query_params: Option<String>,
+        }
+    }
+    
+    pub mod http_method {
+        pub enum HTTPMethod {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            PATCH,
+            OPTIONS,
+            HEAD,
+            TRACE,
+            CONNECT,
+        }
     }
 }
