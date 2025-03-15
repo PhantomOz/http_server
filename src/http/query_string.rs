60eff19dea7b2b46e831257f22ff199b4cc46dcc
PhantomOz <faniogor@gmail.com>
2025-03-15 03:47:50 +0100

    This adds the Query_string get fn

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -1,7 +1,16 @@
 use std::collections::HashMap;
 
 pub struct QueryString<'buf> {
-    data: HashMap<&'buf str, &'buf str>,
+    data: HashMap<&'buf str, Value<'buf>>,
 }
 
-impl<'buf> QueryString<'buf> {}
+pub enum Value<'buf> {
+    Single(&'buf str),
+    Multiple(Vec<&'buf str>),
+}
+
+impl<'buf> QueryString<'buf> {
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+}
